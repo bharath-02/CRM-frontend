017b1a33834fd26fa9b7038c3014d34fd1fcf868
Bharath02 <bharath.janakiram@banfico.com>
2021-08-10 21:57:28 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './components/home'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
