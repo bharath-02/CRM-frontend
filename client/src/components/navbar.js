a70ac28a57054c71bf3142afa7de32da8f0e2f2b
Bharath02 <bharath.janakiram@banfico.com>
2021-08-18 22:30:00 +0530

    navbar improvements

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -6,10 +6,10 @@ import {
     Toolbar,
     Typography,
     Button,
-    IconButton
+    IconButton,
+    Link
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -34,13 +34,20 @@ const Navbar = () => {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Contact Keeper
+                        <Link href="/" color="inherit" className="linkStyling">
+                            Contact Keeper
+                        </Link>
                     </Typography>
-                    <Button color="inherit">View</Button>
-                    <Button color="inherit">Create</Button>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <AccountCircle />
-                    </IconButton>
+                    <Button color="inherit">
+                        <Link href="/view" color="inherit" className="linkStyling">
+                            View
+                        </Link>
+                    </Button>
+                    <Button color="inherit">
+                        <Link href="/create" color="inherit" className="linkStyling">
+                            Create
+                        </Link>
+                    </Button>
                 </Toolbar>
             </AppBar>
         </div>
