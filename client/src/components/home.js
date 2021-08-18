d73c77b90752c1f910341cf0c964518c1d089fa4
Bharath02 <bharath.janakiram@banfico.com>
2021-08-18 23:07:30 +0530

    completed homepage

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { 
+    Typography,
+    Button
+} from '@material-ui/core';
+import PermContactCalendarSharpIcon from '@material-ui/icons/PermContactCalendarSharp';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to the page</h1>
+        <div className="flex-items">
+            <PermContactCalendarSharpIcon color="primary" style={{ fontSize: 250, marginTop: 50 }} />
+
+            <Typography variant="h3" color="primary" style={{ marginTop: 30 }}>
+                Manage your contacts here
+            </Typography>
+
+            <div style={{ marginTop: 30 }}>
+                <Button href="/view" variant="contained" color="primary" size="large" style={{ marginRight: 30 }}>
+                    View
+                </Button>
+                <Button href="/create" variant="contained" color="primary" size="large">
+                    Create
+                </Button>
+            </div>
         </div>
     )
 }
